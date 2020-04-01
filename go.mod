7a0a9cd7c6e6b18963e73147ea70b126ee6f96bc
aman-bansal <bansalaman2905@gmail.com>
2020-04-01 23:43:46 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aman-bansal/go_get_set_generator
+
+go 1.13
---
+++ test/sample.go
@@ -0,0 +1,16 @@
+package test
+
+type SampleObject struct {
+	Id          string
+	Name        string
+	Age         int64
+	IsMale      bool
+	AnotherUser SampleObject2
+}
+
+type SampleObject2 struct {
+	Id     string
+	Name   string
+	Age    int64
+	IsMale bool
+}
